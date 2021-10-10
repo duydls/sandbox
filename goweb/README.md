49b69f1b21ae4285ad7c66eacf7dbaf60ef6d9f9
Yi <yidu@yis-macbook-pro.lan>
2021-10-10 11:03:01 -0500

    submit ex_1

--- goweb/README.md
+++ goweb/README.md
@@ -9,7 +9,38 @@ go build goweb.go
 ``` 
  ./goweb
 ```
-3. from another terminal:
+3. http://localhost:8080
 ```
-curl http://localhost:8000
+Hello, there
 ```
+4. http://localhost:8080/header
+````
+Header field "Upgrade-Insecure-Requests", Value ["1"]
+Header field "Accept", Value ["text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8"]
+Header field "User-Agent", Value ["Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.3 Safari/605.1.15"]
+Header field "Accept-Language", Value ["en-us"]
+Header field "Accept-Encoding", Value ["gzip, deflate"]
+Header field "Connection", Value ["keep-alive"]
+````
+5. http://localhost:8080/version   (return "not found" if no value)
+```
+Header field 'VERSION': "not found" 
+```
+6. http://localhost:8080/healthz
+```
+200
+```
+7. http://localhost:8080/log
+```
+Hello, [::1]:60094
+
+```
+
+8. check local log file
+```
+# cat access.log
+2021/10/10 11:00:03 Handling request for /log/ from [::1]:60094, status: 200
+```
+
+9. TODO: show "404 page not found" when they try a wrong URL
+
--- goweb/goweb.go
+++ goweb/goweb.go
@@ -1,20 +1,83 @@
 package main
 
 import (
-    "fmt"
-    "log"
-    "net/http"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
 )
 
+type StatusRecorder struct {
+	http.ResponseWriter
+	Status int
+}
+
+func (r *StatusRecorder) WriteHeader(status int) {
+	r.Status = status
+	r.ResponseWriter.WriteHeader(status)
+}
+
+func WithLogging(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		recorder := &StatusRecorder{
+			ResponseWriter: w,
+			Status:         200,
+		}
+		h.ServeHTTP(recorder, r)
+		log.Printf("Handling request for %s from %s, status: %d", r.URL.Path, r.RemoteAddr, recorder.Status)
+	})
+}
+
 func main() {
-    http.HandleFunc("/", handler)
-    log.Fatal(http.ListenAndServe("localhost:8000", nil))
+	// define log
+	fn := "access.log"
+	logFile, err := os.Create(fn)
+	defer logFile.Close()
+	if err != nil {
+		log.Fatalln("Open file error!")
+	}
+	log.SetOutput(logFile)
+	myHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, %s", r.RemoteAddr)
+	})
+	handlerWithLogging := WithLogging(myHandler)
+	http.Handle("/log/", handlerWithLogging)
+	//define handles
+	http.HandleFunc("/", HelloHandler)
+	http.HandleFunc("/header/", handler)
+	http.HandleFunc("/version/", systemvar)
+	http.HandleFunc("/healthz/", healthz)
+	fmt.Println("Server started at port 8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "%s %s %s \n", r.Method, r.URL, r.Proto)
-    //Iterate over all header fields
-    for k, v := range r.Header {
-        fmt.Fprintf(w, "Header field %q, Value %q\n", k, v)
-    }
+	//fmt.Fprintf(w, "%s %s %s \n", r.Method, r.URL, r.Proto)
+	//Iterate over all header fields
+	for k, v := range r.Header {
+		w.Header().Set(k, v[0])
+
+	}
+	//w.Header().Set("mykey", "myvalue")
+	for m, n := range w.Header() {
+		fmt.Fprintf(w, "Header field %q, Value %q\n", m, n)
+	}
+}
+
+func systemvar(w http.ResponseWriter, r *http.Request) {
+	version := os.Getenv("VERSION")
+	if version == "" {
+		version = "not found"
+	}
+	w.Header().Set("VERSION", version)
+	fmt.Fprintf(w, "Header field 'VERSION': %q ", version)
+}
+
+func HelloHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, there\n")
+}
+
+func healthz(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "200\n")
 }
